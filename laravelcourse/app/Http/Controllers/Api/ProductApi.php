bcad73c7cd8de106ac96273cf30cbb2f2612782c
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-09-19 23:46:18 +0200

    # Workshop 4 - API REST en Laravel Si problème avec "Target class [] does not exist" : penser à vérifier les chemins des routes.

---
+++ laravelcourse/app/Http/Controllers/Api/ProductApi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+// use Illuminate\Routing\Controller;
+
+class ProductApi extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+}
