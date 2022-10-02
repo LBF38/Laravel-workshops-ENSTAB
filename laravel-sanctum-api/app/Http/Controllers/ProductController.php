309c6c281592a37397152d3e90c61192c2eee620
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-02 14:15:35 +0200

    # Workshop 5 - Sanctum Authentication REST API
    Finalisation de l'API pour les produits :
    - création méthode Update
    - création méthode Destroy
    - création méthode Search
    Création des routes manquantes (search)
    Vérification via POSTMAN du bon fonctionnement de l'API.

--- laravel-sanctum-api/app/Http/Controllers/ProductController.php
+++ laravel-sanctum-api/app/Http/Controllers/ProductController.php
@@ -53,7 +53,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
     }
 
     /**
@@ -64,6 +66,17 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Product::destroy($id);
+    }
+
+    /**
+     * Search for a name
+     *
+     * @param  str
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Product::where('name','like','%'.$name.'%')->get();
     }
 }
