ae572ed16846dd7cc51182f52b986e1d5da43b87
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-02 15:38:11 +0200

    # Workshop 5 - Sanctum Authentication REST API
    Ajout du AuthController et des méthodes pour register, login, logout
    Vérification du bon fonctionnement en utilisant PostMan
    Ajout de nouvelles routes protégées et de routes publiques
    en fonction des actions de l'utilisateur (register, login, logout)
    
    Fin du workshop 5
    #endofworkshop

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products',[ProductController::class,'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 
 
 
@@ -30,4 +33,5 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products',[ProductController::class,'store']);
     Route::put('/products/{id}',[ProductController::class,'update']);
     Route::delete('/products/{id}',[ProductController::class,'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
