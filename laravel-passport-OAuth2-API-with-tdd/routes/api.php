c6b050f05d0e46f884334fb0d0f5f11535067fcd
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-30 16:11:16 +0100

    Finished setting up the OAuth2 api

--- laravel-passport-OAuth2-API-with-tdd/routes/api.php
+++ laravel-passport-OAuth2-API-with-tdd/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,10 +12,15 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "API" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('/product', ProductController::class)->middleware('auth:api');
